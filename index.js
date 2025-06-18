ba69ce0233a5bfd6cebbe0e21338343603f2f40a
Hadiya <hadiyashaikh2006@gmail.com>
2025-06-18 12:27:33 +0530

    final

--- index.js
+++ index.js
@@ -1,43 +1,82 @@
+// // import {Server} from "socket.io";
+
+// // const io = new Server(9000, {
+// //     cors: {
+// //         origin: "http://localhost:3000",
+// //     }
+// // })
+
+// // io.on("connection", (socket) => {
+// //      console.log("user connected")
+// // });
 // import {Server} from "socket.io";
+
 
 // const io = new Server(9000, {
 //     cors: {
-//         origin: "http://localhost:3000",
+//         origin: ["http://localhost:3000",
+//                "https://whatsapp-client-main.vercel.app",
+//         ]
 //     }
 // })
+// let users = [];
+// const addUser = (userData, socketId) =>{
+//     !users.some(user => user.sub === userData.sub) && 
+//     users.push({ ...userData, socketId });
+//     // console.log(users);
+// }
+// const getUsers = (userId) => {
+//     return users.find(user => user.sub === userId);
+// }
+
 
 // io.on("connection", (socket) => {
 //      console.log("user connected")
-// });
-import {Server} from "socket.io";
 
+//      socket.on("addUser", userData=>{
+//         addUser(userData, socket.id);
+//         io.emit("getUsers", users);
+//      })
+
+//      socket.on("sendMessage",data=>{
+//         const user = getUsers(data.receiverId);
+//         io.to(user.socketId).emit("getMessage", data)
+//      })
+// });
+import { Server } from "socket.io";
 
 const io = new Server(9000, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: [
+            "http://localhost:3000",
+            "https://whatsapp-client-main.vercel.app",
+            
+        ],
+        methods: ["GET", "POST"]
     }
-})
+});
+
 let users = [];
-const addUser = (userData, socketId) =>{
-    !users.some(user => user.sub === userData.sub) && 
-    users.push({ ...userData, socketId });
-    // console.log(users);
-}
+const addUser = (userData, socketId) => {
+    !users.some(user => user.sub === userData.sub) &&
+        users.push({ ...userData, socketId });
+};
 const getUsers = (userId) => {
     return users.find(user => user.sub === userId);
-}
-
+};
 
 io.on("connection", (socket) => {
-     console.log("user connected")
+    console.log("user connected");
 
-     socket.on("addUser", userData=>{
+    socket.on("addUser", userData => {
         addUser(userData, socket.id);
         io.emit("getUsers", users);
-     })
+    });
 
-     socket.on("sendMessage",data=>{
+    socket.on("sendMessage", data => {
         const user = getUsers(data.receiverId);
-        io.to(user.socketId).emit("getMessage", data)
-     })
+        if (user) {
+            io.to(user.socketId).emit("getMessage", data);
+        }
+    });
 });
